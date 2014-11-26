3a75a8d441166ad386c01a774b32be15a21a1e25
Māris Krivtežs <marisks@gmail.com>
2014-11-26 10:53:44 +0200

    Added shipping client with settings

---
+++ Geta.Bring.Shipping/ShippingClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Geta.Bring.Shipping
+{
+    public class ShippingClient
+    {
+        public ShippingSettings Settings { get; private set; }
+
+        public ShippingClient(ShippingSettings settings)
+        {
+            if (settings == null) throw new ArgumentNullException("settings");
+            Settings = settings;
+        }
+    }
+}
---
+++ Geta.Bring.Shipping/ShippingSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace Geta.Bring.Shipping
+{
+    public class ShippingSettings
+    {
+         
+    }
+}
