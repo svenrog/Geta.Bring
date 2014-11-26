0df33ef615c18173a0dc1b68dcc917cd174f43ac
Māris Krivtežs <marisks@gmail.com>
2014-11-26 12:40:38 +0200

    Added additional service

---
+++ Geta.Bring.Shipping/Model/AdditionalService.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+
+namespace Geta.Bring.Shipping.Model
+{
+    public class AdditionalService
+    {
+        private AdditionalService(string displayName, string code, IEnumerable<Product> appliesTo)
+        {
+            AppliesTo = appliesTo;
+            Code = code;
+            DisplayName = displayName;
+        }
+
+        public string DisplayName { get; private set; }
+        public string Code { get; private set; }
+        public IEnumerable<Product> AppliesTo { get; private set; }
+
+        public static AdditionalService Evarsling = 
+            new AdditionalService("Recipient notification over SMS or E-Mail", "EVARSLING", new[]
+            {
+                Product.BpakkeDorDor,
+                Product.Servicepakke,
+                Product.Express09
+            });
+        public static AdditionalService Postoppkrav =
+            new AdditionalService("Cash on Delivery", "POSTOPPKRAV", new[]
+            {
+                Product.APost,
+                Product.BPost,
+                Product.BpakkeDorDor,
+                Product.Servicepakke,
+                Product.PaDoren,
+                Product.Express09
+            });
+        public static AdditionalService Lordagsutkjoring =
+            new AdditionalService("Delivery on Saturdays", "LORDAGSUTKJORING", new[]
+            {
+                Product.Express09
+            });
+        public static AdditionalService Envelope = 
+            new AdditionalService("QuickPack Envelope", "ENVELOPE", new[]
+            {
+                Product.QuickpackOverNight0900,
+                Product.QuickpackOverNight1200,
+                Product.QuickpackDayCertain
+            });
+        public static AdditionalService Advisering = 
+            new AdditionalService("Bring contacts recipient", "ADVISERING", new[]
+            {
+                Product.CargoGroupage
+            });
+        public static AdditionalService PickupPoint = 
+            new AdditionalService("Delivery to pickup point", "PICKUP_POINT", new[]
+            {
+                Product.CarryonHomeshopping
+            });
+
+        public IEnumerable<AdditionalService> All
+        {
+            get
+            {
+                yield return Evarsling;
+                yield return Postoppkrav;
+                yield return Lordagsutkjoring;
+                yield return Envelope;
+                yield return Advisering;
+                yield return PickupPoint;
+            }
+        }
+    }
+}
