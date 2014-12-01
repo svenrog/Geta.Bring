bc83098d8c5b9bfc89127809c2e441ea8754642a
Māris Krivtežs <marisks@gmail.com>
2014-12-01 13:59:07 +0200

    Added bring shipping rates

---
+++ Geta.Bring.EPi.Commerce/Model/BringShippingRate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Mediachase.Commerce;
+using Mediachase.Commerce.Orders;
+
+namespace Geta.Bring.EPi.Commerce.Model
+{
+    public class BringShippingRate : ShippingRate
+    {
+        public string MainDisplayCategory { get; set; }
+        public string SubDisplayCategory { get; set; }
+        public string Description { get; set; }
+        public string HelpText { get; set; }
+        public string Tip { get; set; }
+        public bool InNorwayShipping { get; set; }
+
+        public BringShippingRate(
+            Guid methodId, 
+            string displayName, 
+            string mainDisplayCategory, 
+            string subDisplayCategory, 
+            string description, 
+            string helpText, 
+            string tip, 
+            Money money) 
+            : base(methodId, displayName, money)
+        {
+            MainDisplayCategory = mainDisplayCategory;
+            SubDisplayCategory = subDisplayCategory;
+            Description = description;
+            HelpText = helpText;
+            Tip = tip;
+        }
+    }
+}
---
+++ Geta.Bring.EPi.Commerce/Model/BringShippingRateGroup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Geta.Bring.EPi.Commerce.Model
+{
+    public class BringShippingRateGroup
+    {
+        public BringShippingRateGroup(string name, IEnumerable<BringShippingRate> shippingRates)
+        {
+            if (name == null) throw new ArgumentNullException("name");
+            if (shippingRates == null) throw new ArgumentNullException("shippingRates");
+            ShippingRates = shippingRates;
+            Name = name;
+        }
+
+        public string Name { get; private set; }
+
+        public IEnumerable<BringShippingRate> ShippingRates { get; private set; }
+    }
+}
