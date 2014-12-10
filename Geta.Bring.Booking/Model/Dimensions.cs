07f0388ed295e95056610ce70ae4fdd58582280c
Māris Krivtežs <marisks@gmail.com>
2014-12-10 11:54:52 +0200

    Added equality for dimensions

--- Geta.Bring.Booking/Model/Dimensions.cs
+++ Geta.Bring.Booking/Model/Dimensions.cs
@@ -10,7 +10,32 @@
         }
 
         public int HeightInCm { get; private set; }
+
         public int WidthInCm { get; private set; }
+
         public int LengthInCm { get; private set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+            return Equals((Dimensions)obj);
+        }
+
+        protected bool Equals(Dimensions other)
+        {
+            return HeightInCm == other.HeightInCm && WidthInCm == other.WidthInCm && LengthInCm == other.LengthInCm;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = HeightInCm;
+                hashCode = (hashCode*397) ^ WidthInCm;
+                hashCode = (hashCode*397) ^ LengthInCm;
+                return hashCode;
+            }
+        }
     }
 }
