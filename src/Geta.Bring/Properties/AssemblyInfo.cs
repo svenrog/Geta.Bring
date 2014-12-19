2a8d2d40924646dd8d68761bab894e92afcb3b18
Māris Krivtežs <marisks@gmail.com>
2014-12-19 11:12:07 +0200

    Updated assembly info

--- src/Geta.Bring/Properties/AssemblyInfo.cs
+++ src/Geta.Bring/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Geta.Bring")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Bring .NET API")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Geta AS")]
 [assembly: AssemblyProduct("Geta.Bring")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © 2014 Geta AS")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
