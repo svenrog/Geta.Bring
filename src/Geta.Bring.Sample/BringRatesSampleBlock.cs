ccd2711d59d2ce455b51da249ac982a49583cca3
Māris Krivtežs <marisks@gmail.com>
2015-02-03 17:18:37 +0200

    Styling of rates

--- src/Geta.Bring.Sample/BringRatesSampleBlock.cs
+++ src/Geta.Bring.Sample/BringRatesSampleBlock.cs
@@ -1,7 +1,13 @@
 ﻿using EPiServer.Core;
+using EPiServer.DataAnnotations;
 
 namespace Geta.Bring.Sample
 {
+    [ContentType(
+        DisplayName = "Bring Shipping Guide Sample Block", 
+        Description = "Bring Shipping sample block to show how rates can be retrieved.", 
+        GUID = "B3B7932B-97A8-4985-A0D5-16664F6A1B57")]
+    [ImageUrl("~/Content/BringRatesSample/posten-logo-block.png")]
     public class BringRatesSampleBlock : BlockData
     {
          
