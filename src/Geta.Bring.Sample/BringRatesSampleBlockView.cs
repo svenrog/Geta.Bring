03392f86a0ed92f851a5fe97a8b0d59f3f029d85
Māris Krivtežs <marisks@gmail.com>
2015-02-04 17:02:30 +0200

    Added search form'

--- src/Geta.Bring.Sample/BringRatesSampleBlockView.cs
+++ src/Geta.Bring.Sample/BringRatesSampleBlockView.cs
@@ -1,15 +1,78 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web.Mvc;
 using Geta.Bring.Shipping.Model;
 
 namespace Geta.Bring.Sample
 {
     public class BringRatesSampleBlockView
     {
+        public ViewState State { get; private set; }
+
+        [Required]
+        public string CountryFrom { get; set; }
+        [Required(ErrorMessage = "Fra postnummer nødvendig.")]
+        public string PostalCodeFrom { get; set; }
+        [Required]
+        public string CountryTo { get; set; }
+        [Required(ErrorMessage = "Til postnummer nødvendig.")]
+        public string PostalCodeTo { get; set; }
+        public DateTime? ShippingDateAndTime { get; set; }
+        public int? Weight { get; set; }
+        public int? Length { get; set; }
+        public int? Width { get; set; }
+        public int? Height { get; set; }
+        public int? Volume { get; set; }
+        public bool Edi { get; set; }
+        public bool ShippedFromPostOffice { get; set; }
+        public string[] Products { get; set; }
+        public string[] AdditionalServices { get; set; }
+
         public IEnumerable<EstimateGroup> EstimateGroups { get; private set; }
+
+        public IEnumerable<SelectListItem> CountriesFromList
+        {
+            get
+            {
+                yield return new SelectListItem { Value = "NO", Text = "Norway" };
+                yield return new SelectListItem { Value = "SE", Text = "Sweden" };
+                yield return new SelectListItem { Value = "FI", Text = "Finland" };
+                yield return new SelectListItem { Value = "DK", Text = "Denmark" };
+            }
+        }
+
+        public IEnumerable<SelectListItem> CountriesToList
+        {
+            get
+            {
+                yield return new SelectListItem { Value = "NO", Text = "Norway" };
+                yield return new SelectListItem { Value = "SE", Text = "Sweden" };
+                yield return new SelectListItem { Value = "FI", Text = "Finland" };
+                yield return new SelectListItem { Value = "DK", Text = "Denmark" };
+            }
+        }
+
+        public IEnumerable<SelectListItem> ProductList
+        {
+            get { return Product.All.Select(x => new SelectListItem { Value = x.Code, Text = x.DisplayName }); }
+        }
+
+        public IEnumerable<SelectListItem> AdditionalServiceList
+        {
+            get { return AdditionalService.All.Select(x => new SelectListItem { Value = x.Code, Text = x.DisplayName }); }
+        }
+
+        public BringRatesSampleBlockView()
+        {
+            State = ViewState.SearchForm;
+            EstimateGroups = Enumerable.Empty<EstimateGroup>();
+        }
 
         public BringRatesSampleBlockView(IEnumerable<EstimateGroup> estimateGroups)
         {
+            State = ViewState.Results;
             EstimateGroups = estimateGroups;
         }
 
@@ -25,6 +88,12 @@ namespace Geta.Bring.Sample
                 MainCategory = mainCategory;
                 Estimates = estimates;
             }
+        }
+
+        public enum ViewState
+        {
+            SearchForm,
+            Results
         }
     }
 }
--- src/Geta.Bring.Sample/BringRatesSampleController.cs
+++ src/Geta.Bring.Sample/BringRatesSampleController.cs
@@ -10,11 +10,48 @@ namespace Geta.Bring.Sample
 {
     public class BringRatesSampleController : BlockController<BringRatesSampleBlock>
     {
+        private const string ViewPath = "~/Views/Shared/Blocks/BringRatesSample.cshtml";
+
         public override ActionResult Index(BringRatesSampleBlock currentContent)
         {
-            // TODO: get URL from HTTP context
-            var settings = new ShippingSettings(new Uri("http://test.localtest.me"));
-            IShippingClient client = new ShippingClient(settings);
+            return PartialView(ViewPath, new BringRatesSampleBlockView());
+        }
+
+        [HttpPost]
+        public ActionResult Post(BringRatesSampleBlockView formData)
+        {
+            Validate(formData);
+            if (!ModelState.IsValid)
+            {
+                return PartialView(ViewPath, formData);
+            }
+
+            var model = Search(formData);
+            return PartialView(ViewPath, model);
+        }
+
+        private void Validate(BringRatesSampleBlockView formData)
+        {
+            ValidatePackageSize(formData);
+        }
+
+        private void ValidatePackageSize(BringRatesSampleBlockView formData)
+        {
+            var hasWeight = formData.Weight.HasValue;
+            var hasDimensions = formData.Width.HasValue && formData.Height.HasValue && formData.Length.HasValue;
+            var hasVolume = formData.Volume.HasValue;
+            var hasPackageSize = hasWeight || hasDimensions || hasVolume;
+
+            if (!hasPackageSize)
+            {
+                ModelState.AddModelError("", "Ingen pakkestørrelse gitt. Vennligst oppgi vekt, volum eller dimensjoner.");
+            }
+        }
+
+        private BringRatesSampleBlockView Search(BringRatesSampleBlockView formData)
+        {
+            var settings = new ShippingSettings(GetBaseUri());
+            var client = new ShippingClient(settings);
 
             var query = new EstimateQuery(new ShipmentLeg("0484", "5600"), PackageSize.InGrams(2500));
             var result = client.FindAsync<ShipmentEstimate>(query).Result;
@@ -24,7 +61,17 @@ namespace Geta.Bring.Sample
                 .Select(x => new BringRatesSampleBlockView.EstimateGroup(x.Key, x));
 
             var model = new BringRatesSampleBlockView(estimateGroups);
-            return PartialView("~/Views/Shared/Blocks/BringRatesSample.cshtml", model);
+            return model;
+        }
+
+        private Uri GetBaseUri()
+        {
+            if (Request.Url == null)
+            {
+                throw new Exception("Request.Url is null.");
+            }
+
+            return new Uri(string.Format("{0}://{1}{2}", Request.Url.Scheme, Request.Url.Authority, Url.Content("~")));
         }
     }
 }
