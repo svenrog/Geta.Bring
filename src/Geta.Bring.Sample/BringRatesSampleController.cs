368359d8d8af625787f78447195cef821c260d8f
Māris Krivtežs <marisks@gmail.com>
2015-02-05 11:34:50 +0200

    Refactoring of view mapping

--- src/Geta.Bring.Sample/BringRatesSampleController.cs
+++ src/Geta.Bring.Sample/BringRatesSampleController.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 using EPiServer.Web.Mvc;
 using Geta.Bring.Shipping;
 using Geta.Bring.Shipping.Model;
-using Geta.Bring.Shipping.Model.QueryParameters;
 
 namespace Geta.Bring.Sample
 {
@@ -54,9 +52,9 @@ namespace Geta.Bring.Sample
             var settings = new ShippingSettings(GetBaseUri());
             var client = new ShippingClient(settings);
 
-            var shipmentLeg = ViewHelper.GetShipmentLeg(formData);
-            var packageSize = ViewHelper.GetPackageSize(formData);
-            var additionalParameters = ViewHelper.GetAdditionalParameters(formData);
+            var shipmentLeg = ParameterMapper.GetShipmentLeg(formData);
+            var packageSize = ParameterMapper.GetPackageSize(formData);
+            var additionalParameters = ParameterMapper.GetAdditionalParameters(formData);
 
             var query = new EstimateQuery(shipmentLeg, packageSize, additionalParameters);
             var result = client.FindAsync<ShipmentEstimate>(query).Result;
@@ -77,92 +75,6 @@ namespace Geta.Bring.Sample
             }
 
             return new Uri(string.Format("{0}://{1}{2}", Request.Url.Scheme, Request.Url.Authority, Url.Content("~")));
-        }
-
-        private static class ViewHelper
-        {
-            public static ShipmentLeg GetShipmentLeg(BringRatesSampleBlockView view)
-            {
-                return new ShipmentLeg(
-                    view.PostalCodeFrom,
-                    view.PostalCodeTo,
-                    view.CountryFrom,
-                    view.CountryTo);
-            }
-
-            public static PackageSize GetPackageSize(BringRatesSampleBlockView view)
-            {
-                return GetPackageSizes(view).First();
-            }
-
-            public static IQueryParameter[] GetAdditionalParameters(BringRatesSampleBlockView view)
-            {
-                var parameters = new List<IQueryParameter>();
-
-                var additionalPackageSizes = GetPackageSizes(view).Skip(1);
-                parameters.AddRange(additionalPackageSizes);
-                parameters.AddRange(GetShippingDateAndTime(view));
-                parameters.AddRange(GetEdi(view));
-                parameters.AddRange(GetShippedFromPostOffice(view));
-                parameters.AddRange(GetProducts(view));
-                parameters.AddRange(GetAdditionalServices(view));
-
-                return parameters.ToArray();
-            }
-
-            private static IEnumerable<AdditionalServices> GetAdditionalServices(BringRatesSampleBlockView view)
-            {
-                var additionalServices = view.AdditionalServices.Select(AdditionalService.GetByCode).ToList();
-
-                return additionalServices.Count != 0
-                    ? new[] { new AdditionalServices(additionalServices.ToArray()) }
-                    : Enumerable.Empty<AdditionalServices>();
-            }
-
-            private static IEnumerable<Products> GetProducts(BringRatesSampleBlockView view)
-            {
-                var products = view.Products.Select(Product.GetByCode).ToList();
-
-                return products.Count != 0
-                    ? new[] { new Products(products.ToArray()) }
-                    : Enumerable.Empty<Products>();
-            }
-
-            private static IEnumerable<ShippedFromPostOffice> GetShippedFromPostOffice(BringRatesSampleBlockView view)
-            {
-                yield return new ShippedFromPostOffice(view.ShippedFromPostOffice);
-            }
-
-            private static IEnumerable<Edi> GetEdi(BringRatesSampleBlockView view)
-            {
-                yield return new Edi(view.Edi);
-            }
-
-            private static IEnumerable<ShippingDateAndTime> GetShippingDateAndTime(BringRatesSampleBlockView view)
-            {
-                if (view.ShippingDateAndTime.HasValue)
-                {
-                    yield return new ShippingDateAndTime(view.ShippingDateAndTime.Value);
-                }
-            }
-
-            private static IEnumerable<PackageSize> GetPackageSizes(BringRatesSampleBlockView view)
-            {
-                if (view.Weight.HasValue)
-                {
-                    yield return PackageSize.InGrams(view.Weight.Value);
-                }
-
-                if (view.Volume.HasValue)
-                {
-                    yield return PackageSize.InVolume(view.Volume.Value);
-                }
-
-                if (view.Length.HasValue && view.Width.HasValue && view.Height.HasValue)
-                {
-                    yield return PackageSize.InDimensions(view.Length.Value, view.Width.Value, view.Height.Value);
-                }
-            }
         }
     }
 }
---
+++ src/Geta.Bring.Sample/ParameterMapper.cs
@@ -0,0 +1,93 @@
+using System.Collections.Generic;
+using System.Linq;
+using Geta.Bring.Shipping.Model;
+using Geta.Bring.Shipping.Model.QueryParameters;
+
+namespace Geta.Bring.Sample
+{
+    public static class ParameterMapper
+    {
+        public static ShipmentLeg GetShipmentLeg(BringRatesSampleBlockView view)
+        {
+            return new ShipmentLeg(
+                view.PostalCodeFrom,
+                view.PostalCodeTo,
+                view.CountryFrom,
+                view.CountryTo);
+        }
+
+        public static PackageSize GetPackageSize(BringRatesSampleBlockView view)
+        {
+            return GetPackageSizes(view).First();
+        }
+
+        public static IQueryParameter[] GetAdditionalParameters(BringRatesSampleBlockView view)
+        {
+            var parameters = new List<IQueryParameter>();
+
+            var additionalPackageSizes = GetPackageSizes(view).Skip(1);
+            parameters.AddRange(additionalPackageSizes);
+            parameters.AddRange(GetShippingDateAndTime(view));
+            parameters.AddRange(GetEdi(view));
+            parameters.AddRange(GetShippedFromPostOffice(view));
+            parameters.AddRange(GetProducts(view));
+            parameters.AddRange(GetAdditionalServices(view));
+
+            return parameters.ToArray();
+        }
+
+        private static IEnumerable<AdditionalServices> GetAdditionalServices(BringRatesSampleBlockView view)
+        {
+            var additionalServices = view.AdditionalServices.Select(AdditionalService.GetByCode).ToList();
+
+            return additionalServices.Count != 0
+                ? new[] { new AdditionalServices(additionalServices.ToArray()) }
+                : Enumerable.Empty<AdditionalServices>();
+        }
+
+        private static IEnumerable<Products> GetProducts(BringRatesSampleBlockView view)
+        {
+            var products = view.Products.Select(Product.GetByCode).ToList();
+
+            return products.Count != 0
+                ? new[] { new Products(products.ToArray()) }
+                : Enumerable.Empty<Products>();
+        }
+
+        private static IEnumerable<ShippedFromPostOffice> GetShippedFromPostOffice(BringRatesSampleBlockView view)
+        {
+            yield return new ShippedFromPostOffice(view.ShippedFromPostOffice);
+        }
+
+        private static IEnumerable<Edi> GetEdi(BringRatesSampleBlockView view)
+        {
+            yield return new Edi(view.Edi);
+        }
+
+        private static IEnumerable<ShippingDateAndTime> GetShippingDateAndTime(BringRatesSampleBlockView view)
+        {
+            if (view.ShippingDateAndTime.HasValue)
+            {
+                yield return new ShippingDateAndTime(view.ShippingDateAndTime.Value);
+            }
+        }
+
+        private static IEnumerable<PackageSize> GetPackageSizes(BringRatesSampleBlockView view)
+        {
+            if (view.Weight.HasValue)
+            {
+                yield return PackageSize.InGrams(view.Weight.Value);
+            }
+
+            if (view.Volume.HasValue)
+            {
+                yield return PackageSize.InVolume(view.Volume.Value);
+            }
+
+            if (view.Length.HasValue && view.Width.HasValue && view.Height.HasValue)
+            {
+                yield return PackageSize.InDimensions(view.Length.Value, view.Width.Value, view.Height.Value);
+            }
+        }
+    }
+}
