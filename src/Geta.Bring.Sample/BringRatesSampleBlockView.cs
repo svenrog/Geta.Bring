f4a4137bbf7f06ce41516379939c72735ae86c9f
Māris Krivtežs <marisks@gmail.com>
2015-02-17 11:08:49 +0200

    Updated UI of sample

--- src/Geta.Bring.Sample/BringRatesSampleBlockView.cs
+++ src/Geta.Bring.Sample/BringRatesSampleBlockView.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web.Mvc;
@@ -12,22 +13,49 @@ namespace Geta.Bring.Sample
         public ViewState State { get; private set; }
 
         [Required]
+        [DisplayName("Fra land")]
         public string CountryFrom { get; set; }
+
         [Required(ErrorMessage = "Fra postnummer nødvendig.")]
+        [DisplayName("Fra postnummer *")]
         public string PostalCodeFrom { get; set; }
+
         [Required]
+        [DisplayName("Til land")]
         public string CountryTo { get; set; }
+
+        [DisplayName("Til postnummer *")]
         [Required(ErrorMessage = "Til postnummer nødvendig.")]
         public string PostalCodeTo { get; set; }
+
+        [DisplayName("Forsendelsesdato")]
         public DateTime? ShippingDateAndTime { get; set; }
+
+        [DisplayName("Vekt (gram)")]
         public int? Weight { get; set; }
+
+        [DisplayName("Lengde (cm)")]
         public int? Length { get; set; }
+
+        [DisplayName("Bredde (cm)")]
         public int? Width { get; set; }
+
+        [DisplayName("Høyde (cm)")]
         public int? Height { get; set; }
+
+        [DisplayName("Volum (liter/dm³)")]
         public int? Volume { get; set; }
+        
+        [DisplayName("EDI")]
         public bool Edi { get; set; }
+
+        [DisplayName("Sendt fra postkontoret")]
         public bool ShippedFromPostOffice { get; set; }
+
+        [DisplayName("Produkter")]
         public string[] Products { get; set; }
+
+        [DisplayName("Tilleggstjenester")]
         public string[] AdditionalServices { get; set; }
 
         public IEnumerable<EstimateGroup> EstimateGroups { get; private set; }
