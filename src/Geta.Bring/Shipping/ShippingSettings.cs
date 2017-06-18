d9cc2ddc2e2c962dedaa8803971abff24f16d51d
Gatis Bergšpics <gatisb@gmail.com>
2017-06-18 12:54:31 +0300

    fixing TC build errors:
    Shipping\ShippingSettings.cs(46, 68): error CS0103: The name 'nameof' does not exist in the current context
    Shipping\ShippingSettings.cs(47, 70): error CS0103: The name 'nameof' does not exist in the current context

--- src/Geta.Bring/Shipping/ShippingSettings.cs
+++ src/Geta.Bring/Shipping/ShippingSettings.cs
@@ -43,8 +43,8 @@ namespace Geta.Bring.Shipping
             string uid = null,
             string key = null)
         {
-            if (clientUri == null) throw new ArgumentNullException(nameof(clientUri));
-            if (endpointUri == null) throw new ArgumentNullException(nameof(endpointUri));
+            if (clientUri == null) throw new ArgumentNullException("clientUri");
+            if (endpointUri == null) throw new ArgumentNullException("endpointUri");
 
             PublicId = publicId;
             EndpointUri = endpointUri;
